4d411ff5c4a088e95239f1d8817a3ecf637b3433
Eric Mora <eric.mora.almena@gmail.com>
2022-06-18 15:19:04 +0200

    apuntes curso Java dalton

---
+++ Cap 1/jss.js
@@ -0,0 +1,45 @@
+dineroCofla = prompt ("Quanto dinero tienes, Cofla?");
+dineroRoberto = prompt ("Quanto dinero tienes, Roberto?");
+dineroPedro = prompt ("Quanto dinero tienes, Pedro?");
+
+if (dineroCofla >= 0.6 && dineroCofla <1) {
+    alert ("Cofla, comprate el helado de agua")
+} else if (dineroCofla >=1 && dineroCofla <1.6) {
+    alert("Cofla, comprate el helado de Crema")
+} else if (dineroCofla >=1.6 && dineroCofla <1.7) {
+    alert("Cofla, comprate el helado de Heladix")
+} else if (dineroCofla >=1.7 && dineroCofla <1.8) {
+    alert("Cofla, comprate el helado de heladovich")
+}  else if (dineroCofla >=1.8 && dineroCofla <2.9) {
+    alert("Cofla, comprate el helado de helardo o el pote de 1/4KG")
+} else {
+    alert("Cofla, pobre de mierda")
+}
+
+if (dineroRoberto >= 0.6 && dineroCofla <1) {
+    alert ("Roberto, comprate el helado de agua")
+} else if (dineroRoberto >=1 && dineroCofla <1.6) {
+    alert("Roberto, comprate el helado de Crema")
+} else if (dineroRoberto >=1.6 && dineroCofla <1.7) {
+    alert("Roberto, comprate el helado de Heladix")
+} else if (dineroRoberto >=1.7 && dineroCofla <1.8) {
+    alert("Roberto, comprate el helado de heladovich")
+}  else if (dineroRoberto >=1.8 && dineroCofla <2.9) {
+    alert("Roberto, comprate el helado de helardo o el pote de 1/4KG")
+} else {
+    alert("Roberto, pobre de mierda")
+}
+
+if (dineroPedro >= 0.6 && dineroCofla <1) {
+    alert ("Pedro, comprate el helado de agua")
+} else if (dineroPedro >=1 && dineroCofla <1.6) {
+    alert("Pedro, comprate el helado de Crema")
+} else if (dineroPedro >=1.6 && dineroCofla <1.7) {
+    alert("Pedro, comprate el helado de Heladix")
+} else if (dineroPedro >=1.7 && dineroCofla <1.8) {
+    alert("Pedro, comprate el helado de heladovich")
+}  else if (dineroPedro >=1.8 && dineroCofla <2.9) {
+    alert("Pedro, comprate el helado de helardo o el pote de 1/4KG")
+} else {
+    alert("Pedro, pobre de mierda")
+}
